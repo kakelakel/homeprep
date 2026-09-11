e23285c3a27cdbceac2da3e69bcd26a106430162
kakelakel <32018143+kakelakel@users.noreply.github.com>
2026-09-11 17:43:48 +0200

    Update __init__.py
    Add HomePrep config entry setup

--- custom_components/homeprep/__init__.py
+++ custom_components/homeprep/__init__.py
@@ -1,1 +1,20 @@
 """HomePrep integration for Home Assistant."""
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+    """Set up HomePrep from a config entry."""
+    return True
+
+
+async def async_unload_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+    """Unload a HomePrep config entry."""
+    return True
