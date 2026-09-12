eb3111384a54c6e78b664a7d80ae61ae8b48cbf7
kakelakel <32018143+kakelakel@users.noreply.github.com>
2026-09-12 18:09:28 +0200

    fix: load Lovelace image and button enhancements

--- custom_components/homeprep/frontend/homeprep-lovelace.js
+++ custom_components/homeprep/frontend/homeprep-lovelace.js
@@ -1,6 +1,7 @@
-import "./homeprep-ui-shared.js?v=2";
-import "./homeprep-card.js?v=2";
-import "./homeprep-dashboard-cards.js?v=2";
-import "./homeprep-manage-card.js?v=2";
-import "./homeprep-tasks-card.js?v=2";
-import "./homeprep-lovelace-images.js?v=1";
+import "./homeprep-ui-shared.js?v=3";
+import "./homeprep-card.js?v=3";
+import "./homeprep-dashboard-cards.js?v=3";
+import "./homeprep-manage-card.js?v=3";
+import "./homeprep-tasks-card.js?v=3";
+import "./homeprep-lovelace-images.js?v=2";
+import "./homeprep-lovelace-buttons.js?v=1";
